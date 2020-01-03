a31b748c934c97dedf96712627443d7a36ced7e5
tryingtokeepup <kl.hawaii@gmail.com>
2020-01-02 23:49:59 -1000

    adding in changes, working tonight since i got my paperwork done

--- components/house.js
+++ components/house.js
@@ -1,7 +1,16 @@
-const House = () => (
-  <div>
-    <h2>House</h2>
-  </div>
-);
+const House = props => {
+  // we are passing props down with {... house} spreading in the index
+  return (
+    <div>
+      <img
+        src={props.picture}
+        width="100%"
+        alt="A picture of the rental house"
+      />
+      <h2>House</h2>
+    </div>
+  );
+};
+
 // implicit returns with arrow fnctions are strange beasts
 export default House;
