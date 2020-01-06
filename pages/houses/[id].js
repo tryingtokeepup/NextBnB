efc3b71ee9f5147460c8f5c9708f646dee497fd1
tryingtokeepup <kl.hawaii@gmail.com>
2020-01-06 05:59:13 -1000

    finished headers, pushing through

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,11 +1,23 @@
+import Head from 'next/head';
 import houses from '../houses.json';
 
 const House = props => (
   <div>
+    <Head>
+      <title>{props.house.title}</title>
+    </Head>
+    <img src={props.house.picture} width="100%" alt="House picture" />
+    <p>
+      {props.house.type} - {props.house.town}
+    </p>
     <p>{props.house.title}</p>
+    <p>
+      {props.house.rating} ({props.house.reviewsCount})
+    </p>
   </div>
 );
 // query is a deconstructed property from the context object
+// we reform the props passed from house.js and filter for only the house that is on the url query string
 House.getInitialProps = ({ query }) => {
   const { id } = query;
   return {
