ca872841a124d25e5063045c7bb6652588c2dbd1
tryingtokeepup <kl.hawaii@gmail.com>
2020-01-08 22:49:57 -1000

    pushing up to fix next.js errors

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -3,51 +3,86 @@ import Layout from '../../components/Layout';
 
 import houses from '../houses.json';
 import DateRangePicker from '../../components/DateRangePicker';
+import { useState } from 'react';
 
-const House = props => (
-  <Layout
-    content={
-      <div className="container">
-        <Head>
-          <title>{props.house.title}</title>
-        </Head>
-        {/* Semantic HTML helps us breakdown the page easier. */}
-        <article>
-          <img src={props.house.picture} width="100%" alt="House picture" />
-          <p>
-            {props.house.type} - {props.house.town}
-          </p>
-          <p>{props.house.title}</p>
-          <p>
-            {props.house.rating} ({props.house.reviewsCount})
-          </p>
-        </article>
-        <aside>
-          <h2>Add dates for prices</h2>
-          {/* when the user changes the date, we want to pass that change up to here! */}
-          <DateRangePicker
-            datesChanged={(startDate, endDate) => {
-              console.log(startDate, endDate);
-            }}
-          />
-        </aside>
+const calcNumberOfNightsBetweenDates = (startDate, endDate) => {
+  const start = new Date(startDate); //clone
+  const end = new Date(endDate); //clone
+  let dayCount = 0;
 
-        {/* Styling */}
-        <style jsx>{`
-          .container {
-            display: grid;
-            grid-template-columns: 60% 40%;
-            grid-gap: 30px;
-          }
-          aside {
-            border: 1px solid #ccc;
-            padding: 20px;
-          }
-        `}</style>
-      </div>
-    }
-  />
-);
+  while (end > start) {
+    dayCount++;
+    start.setDate(start.getDate() + 1);
+  }
+
+  return dayCount;
+};
+
+const House = props => {
+  const [dateChosen, setDateChosen] = useState(false);
+  const [numberOfNightsBetweenDates, setNumberOfNightsBetweenDates] = useState(
+    0
+  );
+
+  return (
+    <Layout
+      content={
+        <div className="container">
+          <Head>
+            <title>{props.house.title}</title>
+          </Head>
+          {/* Semantic HTML helps us breakdown the page easier. */}
+          <article>
+            <img src={props.house.picture} width="100%" alt="House picture" />
+            <p>
+              {props.house.type} - {props.house.town}
+            </p>
+            <p>{props.house.title}</p>
+            <p>
+              {props.house.rating} ({props.house.reviewsCount})
+            </p>
+          </article>
+          <aside>
+            <h2>Add dates for prices</h2>
+            {/* when the user changes the date, we want to pass that change up to here! */}
+            <DateRangePicker
+              datesChanged={(startDate, endDate) => {
+                setNumberOfNightsBetweenDates(
+                  calcNumberOfNightsBetweenDates(startDate, endDate)
+                );
+                setDateChosen(true);
+              }}
+            />
+            {dateChosen && (
+              <div>
+                <h2>Price per night</h2>
+                <p>${props.house.price}</p>
+                <h2>Current total price of booking</h2>
+                <p>
+                  ${(numberOfNightsBetweenDates * props.house.price).toFixed(2)}
+                </p>
+              </div>
+            )}
+          </aside>
+
+          {/* Styling */}
+          <style jsx>{`
+            .container {
+              display: grid;
+              grid-template-columns: 60% 40%;
+              grid-gap: 30px;
+            }
+            aside {
+              border: 1px solid #ccc;
+              padding: 20px;
+            }
+          `}</style>
+        </div>
+      }
+    />
+  );
+};
+
 // query is a deconstructed property from the context object
 // we reform the props passed from house.js and filter for only the house that is on the url query string
 House.getInitialProps = ({ query }) => {
