df8734a2e16a684e2d6ceb1226741c955d53d02f
tryingtokeepup <kl.hawaii@gmail.com>
2020-01-10 23:11:55 -1000

    going to try a different approach in solving the date pciker issue

--- components/DateRangePicker.js
+++ components/DateRangePicker.js
@@ -1,86 +1,13 @@
-import DayPickerInput from 'react-day-picker/DayPickerInput';
-import 'react-day-picker/lib/style.css';
-import { useState } from 'react';
-import { DateUtils } from 'react-day-picker';
-
-import dateFnsFormat from 'date-fns/format';
-import dateFnsParse from 'date-fns/parse';
-
-const parseDate = (str, format, locale) => {
-  const parsed = dateFnsParse(str, format, new Date(), { locale });
-  return DateUtils.isDate(parsed) ? parsed : null;
-};
-
-const formatDate = (date, format, locale) =>
-  dateFnsFormat(date, format, { locale });
-
-const format = 'dd MMM yyyy';
-
-export default () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-
-  return (
-    <div className="date-range-picker-container">
-      <div>
-        <label>From:</label>
-        <DayPickerInput
-          formatDate={formatDate}
-          format={format}
-          parseDate={parseDate}
-          placeholder={`${dateFnsFormat(new Date(), format)}`}
-          dayPickerProps={{
-            modifiers: {
-              disabled: {
-                before: new Date()
-              }
-            }
-          }}
-          onDayChange={day => {
-            setStartDate(day);
-          }}
-        />
-      </div>
-      <div>
-        <label>To:</label>
-        <DayPickerInput
-          formatDate={formatDate}
-          format={format}
-          parseDate={parseDate}
-          placeholder={`${dateFnsFormat(new Date(), format)}`}
-          dayPickerProps={{
-            modifiers: {
-              disabled: {
-                before: new Date()
-              }
-            }
-          }}
-          onDayChange={day => {
-            setEndDate(day);
-          }}
-        />
-      </div>
-      <style jsx>{`
-        .date-range-picker-container div {
-          display: grid;
-          border: 1px solid #ddd;
-          grid-template-columns: 30% 70%;
-          padding: 10px;
-        }
-        label {
-          padding-top: 10px;
-        }
-      `}</style>
-      <style jsx global>{`
-        .DayPickerInput input {
-          width: 120px;
-          padding: 10px;
-          font-size: 16px;
-        }
-      `}</style>
-    </div>
-  );
-};
+import { DayPicker } from 'react-day-picker';
+export default () => (
+  <div>
+    <DayPicker
+      onMonthChange={console.log}
+      onDayClick={console.log}
+      showWeekNumber
+    />
+  </div>
+);
 
 // import { useState } from 'react';
 // import DayPickerInput from 'react-day-picker/DayPickerInput';
