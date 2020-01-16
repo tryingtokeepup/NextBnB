7f31aa0db5561ff4e4c19f737a4ff9a9b0f0cf05
tryingtokeepup <kl.hawaii@gmail.com>
2020-01-16 06:49:05 -1000

    surrounding House component with div

--- components/house.js
+++ components/house.js
@@ -4,23 +4,25 @@ const House = props => {
   // we are passing props down with {... house} spreading in the index
   return (
     // Very cool, we can basically use routing with Next's version of
-    <Link href="/houses/[id]" as={'/houses/' + props.id}>
-      <a>
-        <img
-          src={props.picture}
-          width="100%"
-          alt="A picture of the rental house"
-        />
-        <p>
-          {props.type} - {props.town}
-        </p>
+    <div>
+      <Link href="/houses/[id]" as={'/houses/' + props.id}>
+        <a>
+          <img
+            src={props.picture}
+            width="100%"
+            alt="A picture of the rental house"
+          />
+          <p>
+            {props.type} - {props.town}
+          </p>
 
-        <p>{props.title}</p>
-        <p>
-          {props.rating} ({props.reviewsCount})
-        </p>
-      </a>
-    </Link>
+          <p>{props.title}</p>
+          <p>
+            {props.rating} ({props.reviewsCount})
+          </p>
+        </a>
+      </Link>
+    </div>
   );
 };
 
